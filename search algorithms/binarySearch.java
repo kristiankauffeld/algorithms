58991b39d8a05310dd6603dd2b9a6eaf7a5f90fb
Kristian K. Andreasen <43569402+kristiankauffeld@users.noreply.github.com>
2023-06-21 10:10:31 +0200

    Update binarySearch.java

--- search algorithms/binarySearch.java
+++ search algorithms/binarySearch.java
@@ -1,29 +1,28 @@
 public class Main {
-    public static int binarySearch(int[] sortedArr, int element) {
-        int startIndex = 0;
-        int endIndex = sortedArr.length - 1;
+    public static int bsIterative(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
 
-        while (startIndex <= endIndex) {
-            int middleIndex = startIndex + (endIndex - startIndex) / 2;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
 
-            if (element == sortedArr[middleIndex]) {
-                return middleIndex;
+            if (arr[mid] == target) {
+                return mid;
             }
 
-            if (sortedArr[middleIndex] < element) {
-                startIndex = middleIndex + 1;
-            } else {
-                endIndex = middleIndex - 1;
+            if (arr[mid] < target) {
+                start = mid + 1;
+            }
+
+            if (arr[mid] > target) {
+                end = mid - 1;
             }
         }
         return -1;
     }
 
     public static void main(String[] args) {
-        int[] array = {1, 5, 9, 13, 99, 100};
-
-        int index = binarySearch(array, 99);
-        System.out.println(index);
-
+        int[] arr = {3, 5, 6, 9, 11, 18, 20, 21, 24, 30}; //length is 10
+        System.out.println(bsIterative(arr, 5));
     }
 }
